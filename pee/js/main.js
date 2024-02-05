4401a612711edf1d5fb78352961b6a6a86c6f80d
ShaoShaoUwU <73066684+ShaoShaoUwU@users.noreply.github.com>
2024-02-04 21:19:42 -0500

    03103

--- pee/js/main.js
+++ pee/js/main.js
@@ -5,24 +5,47 @@ const DOMselectors = {
     enter: document.querySelector("#enter"),
     form: document.querySelector(".pop")
 }
+
+const Ships = [];
 form.addEventListener('submit', function (event) {
+
+    const name1 = DOMselectors.name1.value
+    const name2 = DOMselectors.name2.value
+    const loveScore = calculateLoveScore();
+    const newname = makeshipName(name1, name2);
+    Ships.push({ newname, loveScore })
+    console.log(name1, name2)
+
+    function makeshipName(name1, name2) {
+        let shipName = '';
+        const halfName1 = name1.substring(0, Math.ceil(name1.length / 2));
+        const halfName2 = name2.substring(Math.ceil(name2.length / 2));
+        for (let i = 0; i < halfName1.length; i++) {
+            shipName += halfName1[i];
+        }
+        for (let i = 0; i < halfName2.length; i++) {
+            shipName += halfName2[i];
+        }
+        return shipName;
+    }
+    function calculateLoveScore() {
+        const loveScore = Math.floor(Math.random() * 100) + 1;
+        return loveScore;
+    }
+    makeshipName();
+    calculateLoveScore();
     event.preventDefault();
-const name1 = DOMselectors.name1.value
-const name2 = DOMselectors.name2.value
+    ello(Ships);
 
-console.log(name1, name2)
+    function ello(arr) {
+        arr.forEach((ship) =>
+            DOMselectors.galleri.insertAdjacentHTML(
+                "beforeend",
+                `<div class="gall">
+   <h2 class="Name">${ship.newname}</h2>
+   <h3 class="LoveScore">${ship.loveScore}</h3>
+</div>` )
+        );
 
-let shipName = '';
-      const halfName1 = name1.substring(0, Math.ceil(name1.length / 2));
-      const halfName2 = name2.substring(Math.ceil(name2.length / 2));
-      for (let i = 0; i < halfName1.length; i++) {
-        shipName += halfName1[i];
-      }
-      for (let i = 0; i < halfName2.length; i++) {
-        shipName += halfName2[i];
-      }
-
-function calculateLoveScore() {
-        const loveScore = Math.floor(Math.random() * 99) + 1;
-        return loveScore; }
- });
+    }
+});
