c3bc5b72330056659902b7157380053b8164718b
ShaoShaoUwU <73066684+ShaoShaoUwU@users.noreply.github.com>
2024-02-04 20:20:15 -0500

    pop

--- pee/js/main.js
+++ pee/js/main.js
@@ -4,3 +4,8 @@ export const DOMselectors = {
     galleri: document.querySelector(".galleri"),
     enter: document.querySelector("#enter"),
 }
+
+const name1 = DOMselectors.name1.value
+const name2 = DOMselectors.name2.value
+
+console.log(name1, name2)
